c4fbb3161cf30c1f0c960b2e03b41d135daa6cf6
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-06-11 06:10:42 +0200

    *edit* back / chatcontroller : sendMessage & addUser methods

--- back/src/main/java/com/example/chat/controllers/ChatController.java
+++ back/src/main/java/com/example/chat/controllers/ChatController.java
@@ -1,16 +1,27 @@
 package com.example.chat.controllers;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 import com.example.chat.models.ChatMessage;
 
 @Controller
 public class ChatController {
+
     @MessageMapping("/sendMessage")
-    @SendTo("/topic/messages")
-    public ChatMessage sendMessage(ChatMessage message) {
+    @SendTo("/topic/messages") // linked to websocketconfig registry.enableSimpleBroker("/topic/", "/queue/")
+    public ChatMessage sendMessage(@Payload ChatMessage message) {
+        return message;
+    }
+
+    @MessageMapping("/addUser")
+    @SendTo("/topic/messages") // linked to websocketconfig registry.enableSimpleBroker("/topic/", "/queue/")
+    public ChatMessage addUser(@Payload ChatMessage message, SimpMessageHeaderAccessor headerAccessor){
+        // add username to websocket session
+        headerAccessor.getSessionAttributes().put("username", message.getSender());
         return message;
     }
 }
