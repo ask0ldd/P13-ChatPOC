7834ca64dba545054a203888ab6f5d0fae544015
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-06-12 04:18:48 +0200

    *add* front / shared module to serve the whole front

---
+++ front/src/app/module/shared.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],
+  exports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ]
+})
+export class SharedModule { }
