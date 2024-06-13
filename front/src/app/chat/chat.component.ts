5cdfd88e9ea9c36199d20edc0bc92deed22957ed
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-06-13 12:27:34 +0200

    *add* front / main chat component

---
+++ front/src/app/chat/chat.component.ts
@@ -0,0 +1,44 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ChatService } from '../services/chat.service';
+import { IMessage } from '@stomp/stompjs';
+import { IChatMessage } from '../interfaces/IChatMessage';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css']
+})
+export class ChatComponent implements OnInit {
+
+  @ViewChild('messageTextarea')
+  messageTextarea!: ElementRef;
+
+  chatHistory : IChatMessage[] = []
+  subscribed = false
+
+  constructor(
+    private chatService : ChatService, 
+    private authService : AuthService, 
+    private router : Router
+  ){ }
+
+  ngOnInit(): void {
+    if(this.authService.getActiveUser() == "") this.router.navigate(['/'])
+    const displayReceivedMessageCallback = (message : IMessage) => {
+      this.chatHistory.push(JSON.parse(message.body) as IChatMessage)
+    }
+    /*this.chatService.connect(() => {
+      if(!this.subscribed) this.chatService.subscribe("topic/public", callback)
+      this.subscribed = true
+    })*/
+    this.chatService.connect(displayReceivedMessageCallback)
+  }
+
+  sendMessage(){
+    this.chatService.send("/app/chat.sendMessage", this.messageTextarea.nativeElement.value)
+    this.messageTextarea.nativeElement.value = ""
+    console.log(this.chatHistory)
+  }
+}
