df06a562e020ba9effd1d7a020ba4e59bf5804dc
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-06-14 06:24:44 +0200

    *edit* back / renaming the endpoint the websocket is using

--- back/src/main/java/com/example/chat/config/WebSocketConfig.java
+++ back/src/main/java/com/example/chat/config/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
+        registry.setApplicationDestinationPrefixes("/ws");
         registry.enableSimpleBroker("/topic"); // topic linked to chatcontroller @SendTo("/topic/messages"
     }
 
