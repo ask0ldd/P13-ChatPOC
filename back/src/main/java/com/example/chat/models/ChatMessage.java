46b818443b18fb4f18bdbfe474a38c9aef761576
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-06-15 13:19:55 +0200

    *edit* back / modified chatmessage model to serve as db reference obj

--- back/src/main/java/com/example/chat/models/ChatMessage.java
+++ back/src/main/java/com/example/chat/models/ChatMessage.java
@@ -1,17 +1,33 @@
 package com.example.chat.models;
 
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
+@Entity(name = "messages")
+@Data
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class ChatMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
-    private String sender;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User sender;
+
     private MessageType type;
+
+    @CreationTimestamp
     private LocalDateTime sentAt;
 }
