0d826c17a52171fbb79f525da8a6a370ff5effc0
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-06-17 05:57:57 +0200

    *edit* back / app properties : targeting the right database

--- back/src/main/java/com/example/chat/services/interfaces/IMessageService.java
+++ back/src/main/java/com/example/chat/services/interfaces/IMessageService.java
@@ -1,4 +1,9 @@
 package com.example.chat.services.interfaces;
 
+import com.example.chat.exceptions.SessionNotFoundException;
+import com.example.chat.models.ChatMessage;
+import com.example.chat.models.ChatSession;
+
 public interface IMessageService {
+    public ChatMessage saveMessage(ChatMessage chatMessage, String sessionName);
 }
