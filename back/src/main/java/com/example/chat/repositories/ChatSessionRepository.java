d30a1e52bafae461d9c67eb0b0207bbdab763758
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-06-17 05:56:29 +0200

    *edit* chat session & user repositories : new custom methods added

--- back/src/main/java/com/example/chat/repositories/ChatSessionRepository.java
+++ back/src/main/java/com/example/chat/repositories/ChatSessionRepository.java
@@ -1,10 +1,12 @@
 package com.example.chat.repositories;
 
 import com.example.chat.models.ChatSession;
+import com.example.chat.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface ChatSessionRepository extends JpaRepository<ChatSession, Long> {
     Optional<ChatSession> findByName(String sessionName);
+    Optional<ChatSession> findByUser(User user);
 }
--- back/src/main/java/com/example/chat/repositories/UserRepository.java
+++ back/src/main/java/com/example/chat/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package com.example.chat.repositories;
 import com.example.chat.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByName(String name);
 }
