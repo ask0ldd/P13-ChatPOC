4b6e38cd9d9d314002337f37887d5f4cb38efcd5
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-06-17 05:55:16 +0200

    *edit* chatcontroller adding multiple rooms support

--- back/src/main/java/com/example/chat/controllers/ChatController.java
+++ back/src/main/java/com/example/chat/controllers/ChatController.java
@@ -1,7 +1,11 @@
 package com.example.chat.controllers;
 
 import com.example.chat.dtos.payloads.MessageDto;
+import com.example.chat.exceptions.UserNotFoundException;
+import com.example.chat.models.User;
+import com.example.chat.repositories.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -16,14 +20,21 @@ import java.time.LocalDateTime;
 @Slf4j
 public class ChatController {
 
+    private final UserRepository userRepository;
+
+    public ChatController(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public") // linked to websocketconfig registry.enableSimpleBroker("/topic/", "/queue/")
     public ChatMessage sendMessage(@Payload MessageDto message) {
         System.out.println("message : " + message.getContent());
+        User user = userRepository.findByName(message.getSender()).orElseThrow(() -> new UserNotFoundException("Target user cannot be found."));
         return ChatMessage.builder()
                 .type(message.getType())
                 .content(message.getContent())
-                .sender(message.getSender())
+                .sender(user)
                 .sentAt(LocalDateTime.now())
                 .build();
     }
@@ -34,10 +45,42 @@ public class ChatController {
         // add username to websocket session
         headerAccessor.getSessionAttributes().put("username", message.getSender());
         System.out.println("User connected : " + message.getSender());
+        User user = userRepository.findByName(message.getSender()).orElseThrow(() -> new UserNotFoundException("Target user cannot be found."));
         return ChatMessage.builder()
                 .type(message.getType())
                 .content(message.getSender() + " connected.")
-                .sender(message.getSender())
+                .sender(user)
+                .sentAt(LocalDateTime.now())
+                .build();
+    }
+
+    @MessageMapping("/chat/{roomId}/sendMessage")
+    @SendTo("/topic/chat/{roomId}")
+    public ChatMessage sendMessage(@DestinationVariable String roomId, @Payload MessageDto message) {
+        System.out.println("message : " + message.getContent());
+        User user = userRepository.findByName(message.getSender()).orElseThrow(() -> new UserNotFoundException("Target user cannot be found."));
+        return ChatMessage.builder()
+                .type(message.getType())
+                .content(message.getContent())
+                .sender(user)
+                // .roomId(roomId)
+                .sentAt(LocalDateTime.now())
+                .build();
+    }
+
+    @MessageMapping("/chat/{roomId}/addUser")
+    @SendTo("/topic/chat/{roomId}")
+    public ChatMessage addUser(@DestinationVariable String roomId, @Payload MessageDto message, SimpMessageHeaderAccessor headerAccessor) {
+        // add username and roomId to websocket session
+        headerAccessor.getSessionAttributes().put("username", message.getSender());
+        headerAccessor.getSessionAttributes().put("roomId", roomId);
+        System.out.println("User connected : " + message.getSender() + " to room: " + roomId);
+        User user = userRepository.findByName(message.getSender()).orElseThrow(() -> new UserNotFoundException("Target user cannot be found."));
+        return ChatMessage.builder()
+                .type(message.getType())
+                .content(message.getSender() + " connected.")
+                .sender(user)
+                // .roomId(roomId)
                 .sentAt(LocalDateTime.now())
                 .build();
     }
