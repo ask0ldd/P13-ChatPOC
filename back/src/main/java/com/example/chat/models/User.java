7b6f4c440fda16c6e9e4f7bcca4fc49f45f4a410
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-06-18 12:42:33 +0200

    *add* back / user model

--- back/src/main/java/com/example/chat/models/User.java
+++ back/src/main/java/com/example/chat/models/User.java
@@ -24,4 +24,7 @@ public class User {
 
     @NonNull
     private String password;
+
+    @NonNull
+    private UserRole role;
 }
---
+++ back/src/main/java/com/example/chat/models/UserRole.java
@@ -0,0 +1,6 @@
+package com.example.chat.models;
+
+public enum UserRole {
+    ADMIN,
+    USER
+}
