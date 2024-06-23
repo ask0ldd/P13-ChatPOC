1773c9e707803beec9a75d00a877dbe0a069d31d
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-06-23 10:06:16 +0200

    *edit* front / queue service : method to remove a target user or the logged user itself

--- front/src/app/services/queue.service.ts
+++ front/src/app/services/queue.service.ts
@@ -1,14 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IUser } from '../interfaces/IUser';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QueueService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private authService : AuthService) { }
 
   get$(){
-    return this.httpClient.get<any>(`api/queue`)
+    return this.httpClient.get<IUser[]>(`api/queue`)
+  }
+
+  removeUser$(username : string){
+    return this.httpClient.post<IUser[]>('api/queue/remove', username)
+  }
+
+  removeSelf$(){
+    return this.httpClient.post<IUser[]>('api/queue/remove', this.authService.getLoggedUserName())
   }
 }
