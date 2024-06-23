bac00b0cd00d8461694f274acf8781f0dc81a8ba
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-06-23 10:07:04 +0200

    *edit* front / chat component : handling the specificity of the admin role

--- front/src/app/chat/chat.component.ts
+++ front/src/app/chat/chat.component.ts
@@ -5,6 +5,8 @@ import { IChatMessage } from '../interfaces/IChatMessage';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 import { QueueService } from '../services/queue.service';
+import { Subscription, take } from 'rxjs';
+import { IUser } from '../interfaces/IUser';
 
 @Component({
   selector: 'app-chat',
@@ -17,7 +19,12 @@ export class ChatComponent implements OnInit, OnDestroy {
   messageTextarea!: ElementRef;
 
   chatHistory : IChatMessage[] = []
-  subscribed = false
+  // subscribed = false
+  queue : IUser[] = []
+  queueSubscription! : Subscription
+
+  currentRole! : "ADMIN" | "USER"
+  activeCustomerChatroomId = ""
 
   constructor(
     private chatService : ChatService, 
@@ -27,28 +34,51 @@ export class ChatComponent implements OnInit, OnDestroy {
   ){ }
 
   ngOnInit(): void {
-    if(this.authService.getUsername() == "") {
+    if(this.authService.getLoggedUserName() == "") {
       this.router.navigate(['/']) 
     } 
     else {
-      const displayReceivedMessageCallback = (message : IMessage) => {
-        this.chatHistory.push(JSON.parse(message.body) as IChatMessage)
-      }
-      this.chatService.connect(displayReceivedMessageCallback)
+      this.chatService.connect(this.displayReceivedMessageCallback)
+      this.refreshQueue()
+      this.currentRole = this.authService.getLoggedUserRole()
     }
+  }
+
+  // action to trigger when a new message is received
+  displayReceivedMessageCallback = (message : IMessage) => {
+    this.chatHistory.push(JSON.parse(message.body) as IChatMessage)
   }
 
   sendMessage(){
     // this.chatService.sendPublicMessage("/app/chat.sendMessage", this.messageTextarea.nativeElement.value)
-    this.chatService.sendPrivateMessage(this.authService.getUserPrivateRoomId(), this.messageTextarea.nativeElement.value)
+
+    // if admin & customer selected
+    if(this.currentRole == "ADMIN") {
+      if(this.activeCustomerChatroomId != "") this.chatService.sendPrivateMessage(this.activeCustomerChatroomId, this.messageTextarea.nativeElement.value)
+    } else {
+      // if user
+      this.chatService.sendPrivateMessage(this.authService.getLoggedUserPrivateRoomId(), this.messageTextarea.nativeElement.value)
+    }
     this.messageTextarea.nativeElement.value = ""
   }
 
   refreshQueue(){
-    
+    this.queueSubscription = this.queueService.get$().pipe(take(1)).subscribe({
+      next : (data) => {
+        console.log('data : ' + JSON.stringify(data))
+        this.queue = data
+      }
+    })
+  }
+
+  joinRoom(chatroomId : string){
+    this.activeCustomerChatroomId = chatroomId
+    this.chatService.sendPrivateMessage(chatroomId, "An Admin is here to help you.")
   }
 
   ngOnDestroy(): void {
       this.chatService.disconnect()
+      if(this.queueSubscription) this.queueSubscription.unsubscribe()
+      this.queueService.removeSelf$().subscribe().unsubscribe()
   }
 }
