869c8c0e2af42856d39370ce4d679627d6547168
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-06-25 18:25:53 +0200

    *edit* front / chat component : improved displayed users queue

--- front/src/app/chat/chat.component.ts
+++ front/src/app/chat/chat.component.ts
@@ -25,7 +25,9 @@ export class ChatComponent implements OnInit, OnDestroy {
   queueSubscription! : Subscription
 
   currentRole! : TUserRole
-  activeCustomerChatroomId = ""
+  // activeCustomerChatroomId = ""
+
+  assignedCustomer : IUser | null = null
 
   constructor(
     private chatService : ChatService, 
@@ -47,36 +49,44 @@ export class ChatComponent implements OnInit, OnDestroy {
 
   // action to trigger when a new message is received
   displayReceivedMessageCallback = (message : IMessage) => {
-    console.log(JSON.stringify(message.body))
+    // console.log(JSON.stringify(message.body))
     this.chatHistory.push(JSON.parse(message.body) as IChatMessage)
   }
 
   sendMessage(){
     // if admin & customer selected
     if(this.currentRole == "ADMIN") {
-      if(this.activeCustomerChatroomId != "") this.chatService.sendMessage({isPrivate : true, type : "CHAT"}, this.messageTextarea.nativeElement.value, this.activeCustomerChatroomId)
+      if(this.assignedCustomer?.chatroomId != "") this.chatService.sendMessage({isMessagePrivate : true, type : "CHAT"}, this.messageTextarea.nativeElement.value, this.assignedCustomer?.chatroomId)
     } else {
       // if customer
-      this.chatService.sendMessage({isPrivate : true, type : "CHAT"}, this.messageTextarea.nativeElement.value, this.authService.getLoggedUserPrivateRoomId())
+      this.chatService.sendMessage({isMessagePrivate : true, type : "CHAT"}, this.messageTextarea.nativeElement.value, this.authService.getLoggedUserPrivateRoomId())
     }
     this.messageTextarea.nativeElement.value = ""
   }
 
   refreshQueue(){
     this.queueSubscription = this.queueService.get$().pipe(take(1)).subscribe({
-      next : (data) => {
-        console.log('data : ' + JSON.stringify(data))
-        this.queue = data
+      next : (customers) => {
+        // console.log('data : ' + JSON.stringify(data))
+        this.queue = customers
       }
     })
   }
 
   moveToCustomerRoom(chatroomId : string){
-    this.activeCustomerChatroomId = chatroomId
+    // this.activeCustomerChatroomId = chatroomId
     this.chatService.disconnect()
     this.chatService.connect(this.displayReceivedMessageCallback, chatroomId)
     // doesn't display?
-    this.chatService.sendMessage({isPrivate : true, type : "CHAT"}, "An Admin is here to help you.", chatroomId)
+    this.chatService.sendMessage({isMessagePrivate : true, type : "CHAT"}, "An Admin is here to help you.", chatroomId)
+  }
+
+  assignCustomer(customerName : string){
+    const customer = this.queue.find(customer => customer.username == customerName)
+    if(customer != null) {
+      this.assignedCustomer = customer
+      this.moveToCustomerRoom(this.assignedCustomer.chatroomId)
+    }
   }
 
   ngOnDestroy(): void {
