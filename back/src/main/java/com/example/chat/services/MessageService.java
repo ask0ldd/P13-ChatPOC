b30d3d118c7bc63317426875641f470730a703c0
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-06-26 12:36:48 +0200

    *edit* back / message service : better save message method impl

--- back/src/main/java/com/example/chat/services/MessageService.java
+++ back/src/main/java/com/example/chat/services/MessageService.java
@@ -1,10 +1,13 @@
 package com.example.chat.services;
 
-import com.example.chat.exceptions.SessionNotFoundException;
+import com.example.chat.dtos.payloads.MessageDto;
+import com.example.chat.exceptions.UserNotFoundException;
 import com.example.chat.models.ChatMessage;
-import com.example.chat.models.ChatSession;
+import com.example.chat.models.ChatRoom;
+import com.example.chat.models.User;
 import com.example.chat.repositories.ChatMessageRepository;
-import com.example.chat.repositories.ChatSessionRepository;
+import com.example.chat.repositories.ChatRoomRepository;
+import com.example.chat.repositories.UserRepository;
 import com.example.chat.services.interfaces.IMessageService;
 import org.springframework.stereotype.Service;
 
@@ -12,15 +15,26 @@ import org.springframework.stereotype.Service;
 public class MessageService implements IMessageService {
 
     private final ChatMessageRepository chatMessageRepository;
-    private final ChatSessionRepository chatSessionRepository;
+    private final ChatRoomRepository chatRoomRepository;
+    private final UserRepository userRepository;
 
-    public MessageService(ChatMessageRepository chatMessageRepository, ChatSessionRepository chatSessionRepository){
+    public MessageService(ChatMessageRepository chatMessageRepository, ChatRoomRepository chatRoomRepository, UserRepository userRepository){
         this.chatMessageRepository = chatMessageRepository;
-        this.chatSessionRepository = chatSessionRepository;
+        this.chatRoomRepository = chatRoomRepository;
+        this.userRepository = userRepository;
     }
 
-    public ChatMessage saveMessage(ChatMessage chatMessage, String sessionName){
-        ChatSession session = chatSessionRepository.findByName(sessionName).orElseThrow(() -> new SessionNotFoundException("Target session cannot be found."));
+    public ChatMessage saveMessage(String chatRoomId, MessageDto receivedMessage){
+        User user = userRepository.findByChatRoomId(chatRoomId).orElseThrow(() -> new UserNotFoundException("Target user cannot be found."));
+        // ChatSession session = chatSessionRepository.findByUser(user).orElseThrow(() -> new SessionNotFoundException("Target session cannot be found."));
+        ChatRoom chatroom = chatRoomRepository.findByUser(user)
+                .orElseGet(() -> ChatRoom.builder().user(user).build());
+        ChatRoom newChatRoom = chatRoomRepository.save(chatroom);
+        ChatMessage chatMessage = ChatMessage.builder()
+                .sender(user)
+                .type(receivedMessage.getType())
+                .content(receivedMessage.getContent())
+                .chatroom(newChatRoom).build();
         return chatMessageRepository.save(chatMessage);
     }
 }
