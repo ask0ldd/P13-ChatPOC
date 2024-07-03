22aa2267f87369463c57a82b9ff146bf0ebcbb17
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-07-03 14:55:29 +0200

    *edit* back / data.sql : adding the required timestamps into db

--- back/src/main/resources/data.sql
+++ back/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO `users` (`username`, `email`, `password`, `role`, `chatroom_id`) VALUES ('guest', 'guest@poc.com', 'guest', 'CUSTOMER', 'guestroom1');
-INSERT INTO `users` (`username`, `email`, `password`, `role`, `chatroom_id`) VALUES ('guest2', 'guest2@poc.com', 'guest2', 'CUSTOMER', 'guestroom2');
-INSERT INTO `users` (`username`, `email`, `password`, `role`, `chatroom_id`) VALUES ('admin', 'admin@poc.com', 'admin', 'ADMIN', 'adminroom1');
+INSERT INTO `users` (`username`, `email`, `password`, `role`, `chatroom_id`, `created_at`, `updated_at`) VALUES ('guest', 'guest@poc.com', 'guest', 'CUSTOMER', 'guestroom1', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+INSERT INTO `users` (`username`, `email`, `password`, `role`, `chatroom_id`, `created_at`, `updated_at`) VALUES ('guest2', 'guest2@poc.com', 'guest2', 'CUSTOMER', 'guestroom2', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+INSERT INTO `users` (`username`, `email`, `password`, `role`, `chatroom_id`, `created_at`, `updated_at`) VALUES ('admin', 'admin@poc.com', 'admin', 'ADMIN', 'adminroom1', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
