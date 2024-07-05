fd0a82499a968d0e0d7a3407c37b67ee086c18a4
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-07-05 13:03:03 +0200

    *edit* front / queue service : jsdoc added

--- front/src/app/services/queue.service.ts
+++ front/src/app/services/queue.service.ts
@@ -13,9 +13,20 @@ export class QueueService {
   public queue$ = this.queueSubject.asObservable();*/
   public queue$ = new BehaviorSubject<IUser[]>([]);
 
+  /**
+   * @constructor
+   * @param {HttpClient} httpClient - The HTTP client for making API requests.
+   * @param {AuthService} authService - The authentication service for user-related operations.
+   */
   constructor(private httpClient: HttpClient, private authService : AuthService) { }
 
-  startPolling() {
+  /**
+   * @public
+   * @method startPolling
+   * @description Starts polling the queue every 15 seconds and updates the queue$ BehaviorSubject.
+   * @returns {void}
+   */
+  startPolling() : void {
     timer(0, 15000) // Poll every 15 seconds
       .pipe(
         switchMap(() => this.fetchQueue()),
@@ -26,18 +37,37 @@ export class QueueService {
       })
   }
 
-  private fetchQueue(): Observable<IUser[]>{
+  /**
+   * @private
+   * @method fetchQueue
+   * @description Fetches the current queue from the API.
+   * @returns {Observable<IUser[]>} An observable of the user queue.
+   */
+  private fetchQueue() : Observable<IUser[]>{
     return this.httpClient.get<IUser[]>(`api/queue`)
   }
 
-  removeUser(username : string){
+  /**
+   * @public
+   * @method removeUser
+   * @description Removes a user from the queue by their username.
+   * @param {string} username - The username of the user to remove.
+   * @returns {void}
+   */
+  removeUser(username : string) : void{
     this.httpClient.post<IUser[]>('api/queue/remove', {username : username}).subscribe({
       next: (customers) => this.queue$.next(customers),
       error: () => console.log("can't retrieve the queue.")
     })
   }
 
-  removeSelf$(){
+  /**
+   * @public
+   * @method removeSelf$
+   * @description Removes the currently logged-in user from the queue.
+   * @returns {Observable<IUser[]>} An observable of the updated user queue.
+   */
+  removeSelf$() : Observable<IUser[]>{
     return this.httpClient.post<IUser[]>('api/queue/remove', {username : this.authService.getLoggedUserName()})
   }
   
