1b4a8bc3b7e97f41f976679a3d77d965ead7650a
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-07-24 19:48:47 +0200

    *add* front / assisted customers service

---
+++ front/src/app/services/assisted-customers.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { IUser } from '../interfaces/IUser';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AssistedCustomersService {
+
+  private assistedCustomers : Array<IUser> = []
+
+  constructor() { }
+
+  get list$() : Observable<Array<IUser>>{
+    return of(this.assistedCustomers)
+  }
+
+  addToList(customer : IUser){
+    this.assistedCustomers.push(customer)
+  }
+
+  removeFromList(customer : IUser){
+    this.assistedCustomers = this.assistedCustomers.filter(assistedCustomer => !(assistedCustomer == customer))
+  }
+}
