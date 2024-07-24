99b4aa22eb09037afcf6ee2293e572c6b65817fe
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-07-24 19:49:44 +0200

    *edit* front / assisted component logic updated

--- front/src/app/chat/assisted-customers-list/assisted-customers-list.component.ts
+++ front/src/app/chat/assisted-customers-list/assisted-customers-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { IUser } from 'src/app/interfaces/IUser';
+import { AssistedCustomersService } from 'src/app/services/assisted-customers.service';
 
 @Component({
   selector: 'app-assisted-customers-list',
   templateUrl: './assisted-customers-list.component.html',
   styleUrls: ['./assisted-customers-list.component.css']
 })
-export class AssistedCustomersListComponent {
+export class AssistedCustomersListComponent implements OnDestroy {
+  private assistedCustomersSubscription!: Subscription
+  assistedCustomers : Array<IUser> = []
 
+  constructor(
+    private assistedCustomersService : AssistedCustomersService)
+  {
+    this.assistedCustomersSubscription = this.assistedCustomersService.list$.subscribe(assistedCustomers => this.assistedCustomers = assistedCustomers)
+  }
+
+  ngOnDestroy(): void {
+    if(this.assistedCustomersSubscription) this.assistedCustomersSubscription.unsubscribe()
+  }
 }
