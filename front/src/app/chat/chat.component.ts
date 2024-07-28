74aeadaccaff31c79aa66d4fd022bd582ed1283b
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-07-28 19:17:25 +0200

    *edit* front / chat component : conversation history not delegated anymore

--- front/src/app/chat/chat.component.ts
+++ front/src/app/chat/chat.component.ts
@@ -22,27 +22,28 @@ export class ChatComponent implements OnInit, OnDestroy {
   @ViewChild('messageTextarea')
   messageTextarea!: ElementRef;
 
-  chatHistory : IChatMessage[] = []
+  // chatHistory : IChatMessage[] = []
   queue : IUser[] = []
-  private inMemoryHistorySubscription! : Subscription
+  // private inMemoryHistorySubscription! : Subscription
+  private conversationSubscription! : Subscription
   private queueSubscription! : Subscription
   private timerSubscription!: Subscription
 
   currentRole! : TUserRole
 
   activeCustomer : IUser | null = null
+  activeRoomId = ""
+  activeConversation : IChatMessage[] = []
 
   constructor(
     private chatService : ChatService, 
     private authService : AuthService, 
     private queueService : QueueService,
-    private chatSessionService : ChatSessionService,
     private assistedCustomersService : AssistedCustomersService,
-    private chatNotificationsService : ChatNotificationsService,
     private router : Router)
   {
     this.queueSubscription = this.queueService.queue$.subscribe(queue => this.queue = queue)
-    this.inMemoryHistorySubscription = this.chatSessionService.inMemoryHistory$.subscribe(history => this.chatHistory = history)
+    // this.inMemoryHistorySubscription = this.chatSessionService.inMemoryHistory$.subscribe(history => this.chatHistory = history)
   }
 
   ngOnInit(): void {
@@ -51,26 +52,27 @@ export class ChatComponent implements OnInit, OnDestroy {
       this.router.navigate(['/']) 
     } 
     else {
+      this.activeRoomId = this.authService.getLoggedUserPrivateRoomId()
       // by default, the user is connected to its own private chatroom
-      // this.chatService.connectToChatroom(this.displayReceivedMessageCallback, this.authService.getLoggedUserPrivateRoomId())
-      this.chatService.initChatClient(this.receivedMessageCallback, this.authService.getLoggedUserPrivateRoomId())
+      // this.chatService.connectToChatroom(this.displayReceivedMessageCallback, this.activeRoomId)
+      this.chatService.initChatClient(this.receivedMessageCallback, this.activeRoomId)
       this.currentRole = this.authService.getLoggedUserRole()
       // if the user is an admin, retrieve the queue and autorefresh it every x secs
       if(this.currentRole == "ADMIN") {
         this.queueService.startPolling()
       }
       // retrieve the history for the current chatroom
-      this.chatSessionService.fetchHistory()
+      // this.chatSessionService.fetchHistory()
+      this.conversationSubscription = this.chatService.fetchHistory$(this.activeRoomId).pipe(take(1)).subscribe({
+        next : (chatRoomHistory) => this.activeConversation = chatRoomHistory.messages,
+        error : () => this.activeConversation = []
+      })
     }
   }
 
-  /**
-   * Callback function to handle received messages.
-   * Adds the message to the chat history and resets the inactivity timer.
-   * @param message - The received message object.
-   */
   receivedMessageCallback = (message : IMessage) => {
-    this.chatSessionService.pushToHistory(message)
+    // this.chatSessionService.pushToHistory(message)
+    this.activeConversation.push(JSON.parse(message.body) as IChatMessage)
     // if(this.authService.getLoggedUserRole() == "ADMIN") this.chatNotificationsService.pushNotification(this.authService.getLoggedUserPrivateRoomId())
     this.resetInactivityTimer()
   }
@@ -79,7 +81,7 @@ export class ChatComponent implements OnInit, OnDestroy {
    * Sends a message to the appropriate chatroom based on the user's role and assigned customer.
    */
   sendMessage(){
-    // if the user is an admin with a customer assigned, send the message to the assigned customer's room
+    // if the user is an admin with a customer being assigned, send the message to the assigned customer's room
     if(this.currentRole == "ADMIN" && this.activeCustomer != null) {
       this.chatService.sendMessage("CHAT", this.messageTextarea.nativeElement.value, this.activeCustomer.chatroomId)
     } else {
@@ -88,6 +90,10 @@ export class ChatComponent implements OnInit, OnDestroy {
     }
     this.messageTextarea.nativeElement.value = ""
     this.resetInactivityTimer()
+  }
+
+  switchConversation(){
+    // fetch messages
   }
 
 
@@ -153,7 +159,8 @@ export class ChatComponent implements OnInit, OnDestroy {
       this.chatService.disconnect()
       if(this.timerSubscription) this.timerSubscription.unsubscribe()
       if(this.queueSubscription) this.queueSubscription.unsubscribe()
-      if(this.inMemoryHistorySubscription) this.inMemoryHistorySubscription.unsubscribe()
+      // if(this.inMemoryHistorySubscription) this.inMemoryHistorySubscription.unsubscribe()
+      if(this.conversationSubscription) this.conversationSubscription.unsubscribe()
       this.queueService.removeSelf$().pipe(take(1)).subscribe().unsubscribe()
   }
 }
