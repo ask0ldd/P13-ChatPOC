60c0eb358204f3768a52230157e43273e494957c
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-07-29 15:04:53 +0200

    *edit* front / assisted customer list : using chat component switch conversation instead of conversation session service

--- front/src/app/chat/assisted-customers-list/assisted-customers-list.component.ts
+++ front/src/app/chat/assisted-customers-list/assisted-customers-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { IUser } from 'src/app/interfaces/IUser';
 import { AssistedCustomersService } from 'src/app/services/assisted-customers.service';
@@ -10,19 +10,26 @@ import { ChatSessionService } from 'src/app/services/chat-session.service';
   styleUrls: ['./assisted-customers-list.component.css']
 })
 export class AssistedCustomersListComponent implements OnDestroy {
+
+  @Input() switchConversation : ((customer: IUser) => void) | undefined
+
   private assistedCustomersSubscription!: Subscription
   assistedCustomers : Array<IUser> = []
 
   constructor(
     private assistedCustomersService : AssistedCustomersService,
-    private chatSessionService : ChatSessionService,
+    // private chatSessionService : ChatSessionService,
   )
   {
     this.assistedCustomersSubscription = this.assistedCustomersService.list$.subscribe(assistedCustomers => this.assistedCustomers = assistedCustomers)
   }
 
-  setActiveChatroom(chatroomId : string){
-    this.chatSessionService.setActiveChatroom(chatroomId)
+  /*setActiveChatroom(chatroomId : string){
+    this.chatSessionService.setActiveChatroom(chatroomId) // replace
+  }*/
+
+  executeSwitchConversation(customer : IUser){
+    if(this.switchConversation) this.switchConversation(customer)
   }
 
   ngOnDestroy(): void {
