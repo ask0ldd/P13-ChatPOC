c611739ae994b90c91002424bd08afb33861aa3a
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-08-03 14:16:23 +0200

    *edit* front / jsdoc added for the chat notifications service

--- front/src/app/services/chat-notifications.service.ts
+++ front/src/app/services/chat-notifications.service.ts
@@ -11,11 +11,19 @@ export class ChatNotificationsService {
 
   constructor() { }
 
+  /**
+   * Adds a new chatroom ID to the notifications set and emits the updated set.
+   * @param {string} chatroomId - The ID of the chatroom to add to notifications.
+   */
   pushNotification(chatroomId : string){
     this.notifications.add(chatroomId)
     this.notifications$.next(this.notifications)
   }
 
+  /**
+   * Removes a chatroom ID from the notifications set and emits the updated set.
+   * @param {string} chatroomId - The ID of the chatroom to remove from notifications.
+   */
   pullNotification(chatroomId : string){
     this.notifications.delete(chatroomId)
     this.notifications$.next(this.notifications)
