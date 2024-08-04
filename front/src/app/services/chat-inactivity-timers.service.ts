13391b0c7c6d14731321962436f53d5f129b2bde
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-08-04 13:39:24 +0200

    *edit* front / chat inactivity timers refactored

--- front/src/app/services/chat-inactivity-timers.service.ts
+++ front/src/app/services/chat-inactivity-timers.service.ts
@@ -1,23 +1,20 @@
 import { Injectable } from '@angular/core';
-import { Subscription, timer } from 'rxjs';
-import { ITimerSubs } from '../interfaces/ITimerSubs';
+import { timer } from 'rxjs';
+import { IChatTimer } from '../interfaces/IChatTimer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatInactivityTimersService {
 
-  chatroomsInactivityTimers = new Set<ITimerSubs>()
+  // chatroomsInactivityTimers = new Set<IChatTimer>()
   oneMinute : number = 60000
+  INACTIVITY_TIMEOUT = 5 * this.oneMinute
+  private timers : Map<string, IChatTimer> = new Map();
 
   constructor() { }
 
-  /**
-   * Starts an inactivity timer for a chatroom.
-   * @param {string} chatroomId - The ID of the chatroom.
-   * @param {number} [minutes=5] - The number of minutes for the inactivity timer.
-   */
-  startInactivityTimer(chatroomId : string, minutes : number = 5) {
+  /*startInactivityTimer(chatroomId : string, minutes : number = 5) {
     if([...this.chatroomsInactivityTimers].findIndex(timer => timer.chatroomId == chatroomId) > 0) {
       this.resetInactivityTimer(chatroomId)
       return
@@ -28,20 +25,11 @@ export class ChatInactivityTimersService {
     this.chatroomsInactivityTimers.add({chatroomId : chatroomId, timerSubscription : timerSubscription})
   }
 
-  /**
-   * Resets the inactivity timer for a chatroom.
-   * @param {string} chatroomId - The ID of the chatroom.
-   * @param {number} [minutes=5] - The number of minutes for the inactivity timer.
-   */
   resetInactivityTimer(chatroomId : string, minutes : number = 5) {
     this.killInactivityTimer(chatroomId)
     this.startInactivityTimer(chatroomId, minutes)
   }
 
-  /**
-   * Kills the inactivity timer for a chatroom.
-   * @param {string} chatroomId - The ID of the chatroom.
-   */
   killInactivityTimer(chatroomId : string) {
     const chatroomsInactivityTimersAsArray = [...this.chatroomsInactivityTimers]
     const chatroomTimerIndex = chatroomsInactivityTimersAsArray.findIndex(timer => timer.chatroomId == chatroomId)
@@ -49,14 +37,38 @@ export class ChatInactivityTimersService {
     chatroomsInactivityTimersAsArray[chatroomTimerIndex].timerSubscription.unsubscribe()
     chatroomsInactivityTimersAsArray.splice(chatroomTimerIndex, 1)
     this.chatroomsInactivityTimers = new Set(chatroomsInactivityTimersAsArray)
+  }*/
+
+  startTimer(chatroomId: string): void{
+    this.killTimer(chatroomId)
+    const subscription = timer(this.INACTIVITY_TIMEOUT).subscribe(() => {
+      this.killTimer(chatroomId)
+      this.closeChat(chatroomId)
+    })
+    this.timers.set(chatroomId, { chatroomId, timerSubscription: subscription })
+  }
+
+  killTimer(chatroomId: string): void {
+    const timer = this.timers.get(chatroomId)
+    if (timer) {
+      timer.timerSubscription.unsubscribe()
+      this.timers.delete(chatroomId)
+    }
+  }
+
+  resetTimer(chatroomId: string): void {
+    const timer = this.timers.get(chatroomId)
+    if (timer) {
+      this.startTimer(chatroomId) // kill the timer if existing
+    }
   }
 
   /**
    * Closes the chat for a given chatroom.
    * @param {string} chatroomId - The ID of the chatroom to close.
    */
-  closeChat(chatroomId : string){
-    console.log("Closing the conversation...")
+  closeChat(chatroomId : string) : void{
+    console.log("Closing the conversation...") // should emit back to chat @output()
   }
   
 }
