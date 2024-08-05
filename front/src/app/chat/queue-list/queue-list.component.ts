cf28884a0901c6eebb10fdd7e2fd588afd93271e
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-08-05 19:37:25 +0200

    *edit* front / queue list component jsdoc added

--- front/src/app/chat/queue-list/queue-list.component.ts
+++ front/src/app/chat/queue-list/queue-list.component.ts
@@ -11,6 +11,14 @@ export class QueueListComponent {
   @Input() queue : IUser[] = []
   @Output() callAssignNewCustomerToAdmin = new EventEmitter<IUser>();
 
+  /**
+   * Triggers the assignment of a new customer to an admin.
+   * 
+   * This method emits an event with the specified customer, which can be
+   * handled by the parent component to perform the necessary actions.
+   * 
+   * @param customer - The user object representing the new customer to be assigned.
+   */
   triggerAssignNewCustomerToAdmin(customer : IUser){
     this.callAssignNewCustomerToAdmin.emit(customer)
   }
