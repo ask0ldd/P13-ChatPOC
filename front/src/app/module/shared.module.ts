ea067a94fb93108c9106598b787b2ace6b853436
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-08-05 19:37:44 +0200

    *edit* front / assisted customers service jsdoc added

--- front/src/app/module/shared.module.ts
+++ front/src/app/module/shared.module.ts
@@ -5,8 +5,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
-
 @NgModule({
   declarations: [],
   imports: [
--- front/src/app/services/assisted-customers.service.ts
+++ front/src/app/services/assisted-customers.service.ts
@@ -12,14 +12,29 @@ export class AssistedCustomersService {
 
   constructor() { }
 
+  /**
+   * Gets an observable list of assisted customers.
+   * 
+   * @returns {Observable<Array<IUser>>} An observable that emits the current list of assisted customers.
+   */
   get list$() : Observable<Array<IUser>>{
     return of(this.assistedCustomers)
   }
 
+  /**
+   * Adds a customer to the list of assisted customers.
+   * 
+   * @param {IUser} customer - The customer to be added to the list.
+   */
   addToList(customer : IUser){
     this.assistedCustomers.push(customer)
   }
 
+  /**
+   * Removes a customer from the list of assisted customers.
+   * 
+   * @param {IUser} customer - The customer to be removed from the list.
+   */
   removeFromList(customer : IUser){
     this.assistedCustomers = this.assistedCustomers.filter(assistedCustomer => !(assistedCustomer == customer))
   }
