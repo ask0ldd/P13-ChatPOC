83a7d8ae89db20a68071e793e606599fa929da6b
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-08-16 05:58:15 +0200

    *edit* sql schema / added carpositions to track each car and a refunds table

--- sql_schema/script.sql
+++ sql_schema/script.sql
@@ -18,12 +18,13 @@ CREATE TABLE Users (
     birthdate DATE NOT NULL,
     address_id INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (address_id) REFERENCES Addresses(address_id)
 );
 
 CREATE TABLE Chatrooms (
     chatroom_id INT AUTO_INCREMENT PRIMARY KEY,
-    owner_id INT NOT NULL UNIQUE,
+    owner_id INT NOT NULL UNIQUE, /* a user can only own his private chatroom */
     name VARCHAR(100) NOT NULL UNIQUE,
     FOREIGN KEY (owner_id) REFERENCES Users(user_id)
 );
@@ -38,7 +39,7 @@ CREATE TABLE CarModels (
     model_id INT AUTO_INCREMENT PRIMARY KEY,
     make_id INT NOT NULL,
     model VARCHAR(50) NOT NULL,
-    description VARCHAR(100) NOT NULL,
+    description VARCHAR(255) NOT NULL,
     acriss_category CHAR(1) NOT NULL,
     acriss_type CHAR(1) NOT NULL,
     acriss_transmission CHAR(1) NOT NULL,
@@ -62,39 +63,38 @@ CREATE TABLE Cars (
     FOREIGN KEY (model_id) REFERENCES CarModels(model_id)
 );
 
+/* 2 cars can share the same pictures */
+
 CREATE TABLE CarPictureCarJunction(
     junction_id INT AUTO_INCREMENT PRIMARY KEY,
     picture_id INT NOT NULL,
     car_id INT NOT NULL,
     FOREIGN KEY (picture_id) REFERENCES CarPictures(picture_id),
-    FOREIGN KEY (car_id) REFERENCES Cars(car_id)
+    FOREIGN KEY (car_id) REFERENCES Cars(car_id),
+    UNIQUE(picture_id, car_id)
 );
 
 CREATE TABLE RentalOffices (
     rentaloffice_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL UNIQUE,
     address_id INT NOT NULL,
-    phone_number VARCHAR(20) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL, /* shouldn't be unique, multiple offices can have the same number */
+    email VARCHAR(100) NOT NULL, /* shouldn't be unique, multiple offices can have the same email */
     opening_hours VARCHAR(100),
     FOREIGN KEY (address_id) REFERENCES Addresses(address_id)
 );
 
 CREATE TABLE Pickups (
     pickup_id INT AUTO_INCREMENT PRIMARY KEY,
-    /*rental_id INT NOT NULL,*/
     rentaloffice_id INT NOT NULL,
     date DATE NOT NULL,
-    /*FOREIGN KEY (rental_id) REFERENCES Rentals(rental_id),*/
     FOREIGN KEY (rentaloffice_id) REFERENCES RentalOffices(rentaloffice_id)
 );
 
 CREATE TABLE Dropoffs (
     dropoff_id INT AUTO_INCREMENT PRIMARY KEY,
-    /*rental_id INT NOT NULL,*/
     rentaloffice_id INT NOT NULL,
     date DATE NOT NULL,
-    /*FOREIGN KEY (rental_id) REFERENCES Rentals(rental_id),*/
     FOREIGN KEY (rentaloffice_id) REFERENCES RentalOffices(rentaloffice_id)
 );
 
@@ -105,29 +105,21 @@ CREATE TABLE Rentals (
     pickup_id INT NOT NULL,
     dropoff_id INT NOT NULL,
     total_cost DECIMAL(10, 2) NOT NULL,
-    status ENUM('reserved', 'in_progress', 'completed', 'canceled') DEFAULT 'reserved',
+    status ENUM('RESERVED', 'IN_PROGRESS', 'COMPLETED', 'CANCELED') DEFAULT 'RESERVED',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (car_id) REFERENCES Cars(car_id)
-    FOREIGN KEY (pickup_id) REFERENCES Pickups(pickup_id)
+    FOREIGN KEY (car_id) REFERENCES Cars(car_id),
+    FOREIGN KEY (pickup_id) REFERENCES Pickups(pickup_id),
     FOREIGN KEY (dropoff_id) REFERENCES Dropoffs(dropoff_id)
 );
-
-/*ALTER TABLE Rentals
-ADD CONSTRAINT fk_rental_dropoff
-FOREIGN KEY (dropoff_id) REFERENCES Dropoffs(dropoff_id);
-
-ALTER TABLE Rentals
-ADD CONSTRAINT fk_rental_pickup
-FOREIGN KEY (pickup_id) REFERENCES Pickups(pickup_id);*/
 
 CREATE TABLE ChatMessages (
     chatmessage_id INT AUTO_INCREMENT PRIMARY KEY,
     chatroom_id INT NOT NULL,
     sender_id INT NOT NULL,
     content TEXT NOT NULL,
-    type ENUM('CHAT', 'JOIN', 'LEAVE'),
+    type ENUM('CHAT', 'JOIN', 'LEAVE') DEFAULT 'CHAT',
     sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (chatroom_id) REFERENCES Chatrooms(chatroom_id),
     FOREIGN KEY (sender_id) REFERENCES Users(user_id)
@@ -137,7 +129,7 @@ CREATE TABLE Messages (
     message_id INT AUTO_INCREMENT PRIMARY KEY,
     sender_id INT NOT NULL,
     receiver_id INT NOT NULL,
-    title VARCHAR(255) NOT NULL,
+    title VARCHAR(100) NOT NULL,
     content TEXT NOT NULL,
     attacheddoc_url VARCHAR(255),
     sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -149,7 +141,25 @@ CREATE TABLE Payments (
     payment_id INT AUTO_INCREMENT PRIMARY KEY,
     rental_id INT NOT NULL,
     amount DECIMAL(10, 2) NOT NULL,
-    payment_method ENUM('credit_card', 'debit_card', 'paypal') NOT NULL,
+    payment_method ENUM('CREDIT_CARD', 'DEBIT_CARD', 'PAYPAL') DEFAULT 'CREDIT_CARD',
     payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (rental_id) REFERENCES Rentals(rental_id)
+);
+
+CREATE TABLE Refunds (
+    refund_id INT AUTO_INCREMENT PRIMARY KEY,
+    rental_id INT NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    refund_method ENUM('CREDIT_CARD', 'DEBIT_CARD', 'PAYPAL') DEFAULT 'CREDIT_CARD',
+    refund_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (rental_id) REFERENCES Rentals(rental_id)
+);
+
+CREATE TABLE CarPositions(
+    carposition_id INT AUTO_INCREMENT PRIMARY KEY,
+    car_id INT NOT NULL,
+    rentaloffice_id INT NOT NULL,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (car_id) REFERENCES Cars(car_id),
+    FOREIGN KEY (rentaloffice_id) REFERENCES RentalOffices(rentaloffice_id),
 );
